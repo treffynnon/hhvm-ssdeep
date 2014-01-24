cf76d59d357a119c1b1d11f17934156ee628fb0d
Simon Holywell <simon@emosaic.co.uk>
2014-01-24 13:50:47 +0000

    Flesh out existing extension methods

--- ext_ssdeep.cpp
+++ ext_ssdeep.cpp
@@ -5,25 +5,47 @@
  */
 
 #include "hphp/runtime/base/base-includes.h"
-#include "hphp/runtime/base/zend-string.h"
-
-#include "hphp/runtime/base/runtime-option.h"
-#include "hphp/runtime/server/server-stats.h"
-#include "hphp/runtime/ext/ext_string.h"
-
-#include <cinttypes>
 #include <fuzzy.h>
 
 namespace HPHP {
-    static String HHVM_FUNCTION(ssdeep_info) {
-        return "H";
+    static Variant HHVM_FUNCTION(ssdeep_fuzzy_hash, const String& to_hash) {
+        char* hash = (char*) malloc(FUZZY_MAX_RESULT);
+        int to_hash_len = to_hash.length();
+        const unsigned char* to_hash_char =
+            reinterpret_cast<const unsigned char*>(to_hash.c_str());
+        if (0 != fuzzy_hash_buf(to_hash_char, to_hash_len, hash)) {
+            return false;
+        } else {
+            return String(hash);
+        }
+    }
+
+    static Variant HHVM_FUNCTION(ssdeep_fuzzy_compare, const String& signature1, const String& signature2) {
+        int64_t match;
+        match = fuzzy_compare(signature1.c_str(), signature2.c_str());
+        if(match < 0 || match > 100) {
+            return false;
+        } else {
+            return match;
+        }
+    }
+
+    static Variant HHVM_FUNCTION(ssdeep_fuzzy_hash_filename, const String& file_name) {
+        char* hash = (char*) malloc(FUZZY_MAX_RESULT);
+        if (0 != fuzzy_hash_filename(file_name.c_str(), hash)) {
+            return false;
+        } else {
+            return String(hash);
+        }
     }
 
     class ssdeepExtension: public Extension {
         public:
             ssdeepExtension(): Extension("ssdeep") { /* null */ }
             virtual void moduleInit() {
-                HHVM_FE(ssdeep_info);
+                HHVM_FE(ssdeep_fuzzy_hash);
+                HHVM_FE(ssdeep_fuzzy_compare);
+                HHVM_FE(ssdeep_fuzzy_hash_filename);
                 loadSystemlib();
             }
     } s_ssdeep_extension;
